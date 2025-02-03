4c1431934997f5612ecad33f862b2c912ed34fb5
ozimmortal <zaleambo@gmail.com>
2025-02-03 21:44:36 +0300

    added icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/home/footer";
 
 
 
+
 export const metadata: Metadata = {
   title: "Oliyad | Personal Portfolio",
   description: "This is my Personal Portfolio",
@@ -18,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
       <body className="">
        <ThemeProvider
             attribute="class"
